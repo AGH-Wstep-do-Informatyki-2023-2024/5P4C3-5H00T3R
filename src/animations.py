d73494693c4dc7ad23007fc81c577c4d6d15e9d6
Tomek Styn <32012817+Tombuster@users.noreply.github.com>
2024-01-07 22:48:04 +0100

    Add files via upload

---
+++ src/animations.py
@@ -0,0 +1,149 @@
+import pygame
+from colors import RGB
+
+
+class SpriteSheet:  # przechowuje sprite sheet
+    def __init__(self, spritesheet_image):
+        # load spritesheet image from path
+        self.sheet = spritesheet_image
+
+    def get_image(self, x, width, height, scale, key_col):
+        # make empty surface the size of our sprite
+        image = pygame.Surface((width, height)).convert_alpha()
+
+        # draw an animation frame onto the surface
+        image.blit(self.sheet, (0, 0), ((x * width), 0, width, height))
+
+        # scale the sprite image
+        image = pygame.transform.scale(image, (width * scale, height * scale))
+
+        # keying out the background colour
+        image.set_colorkey(key_col)
+        return image
+
+
+"""
+class Animation:
+    def __init__(self, sprite_sheet_img, sprite_width, sprite_height, scale):
+        self.sprite_sheet = SpriteSheet(sprite_sheet_img)
+        self.sprite_width = sprite_width
+        self.sprite_height = sprite_height
+        self.sprite_scale = scale
+        pass
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
+"""
+
+
+# TODO: dziedziczenie wszystkich klas animacji po tej klasie (maybe...)
+
+
+class GridSheetAnim:  # Animacje na bazie wektora przyspieszenia
+    def __init__(self, sprite_sheet_img, sheet_frames_h, sheet_frames_v, sprite_width, sprite_height, scale):
+
+        # sheet parameters
+        self.sprite_sheet = SpriteSheet(sprite_sheet_img)
+        self.frames_h = sheet_frames_h
+        self.frames_v = sheet_frames_v
+
+        # sprite parameters
+        self.sprite_width = sprite_width
+        self.sprite_height = sprite_height
+        self.sprite_scale = scale
+
+        # frame container and parameters
+        self.current_frame = ()
+        self.frames = []
+
+        # extracting frames from sprite sheet
+        for y in range(self.frames_h):  # numer rzedu w tabeli frames
+            temp_frame_list = []
+            for x in range(self.frames_v):  # indeks w rzedzie w tabeli frames
+                temp_frame_list.append(
+                    self.sprite_sheet.get_image((y * self.frames_h + x), self.sprite_width, self.sprite_height,
+                                                self.sprite_scale, RGB.KEY))
+            self.frames.append(temp_frame_list)
+        pass
+
+    def frame_xy_from_accel_vect(self, accel_vector, transition_threshold):
+        # wybiera ktore klatki wyswietlac na podstawie wektora przyspieszenia
+
+        x, y = (self.frames_h + 1) // 2, (self.frames_v + 1) // 2
+
+        # mapping x component of acceleration vector to frame coordinates (horizontal movement)
+        if accel_vector[0] > transition_threshold:
+            x += 2
+        elif accel_vector[0] > 0:
+            x += 1
+        elif accel_vector[0] == 0:
+            pass
+        elif accel_vector[0] > transition_threshold * -1:
+            x -= 1
+        else:
+            x -= 2
+
+        # mapping y component of acceleration vector to frame coordinates (vertical movement)
+        if accel_vector[1] > transition_threshold:
+            y += 2
+        elif accel_vector[1] > 0:
+            y += 1
+        elif accel_vector[1] == 0:
+            pass
+        elif accel_vector[1] > transition_threshold * -1:
+            y -= 1
+        else:
+            y -= 2
+
+        return x, y
+
+    def update(self, accel_vector, transition_threshold):
+        self.current_frame = self.frame_xy_from_accel_vect(accel_vector, transition_threshold)
+        pass
+
+    def draw(self, screen: pygame.Surface, coords: tuple):
+        screen.blit(self.frames[self.current_frame[1]][self.current_frame[0]], coords)
+
+
+class CyclicAnim:  # Animacje zapetlane - idle, plomenie, wydechy etc. moga miec po kilka wariantow np. zwykly wydech i boost
+    def __init__(self, sprite_sheet_img, frame_counts, frame_time, width, height, scale):
+        # sprite sheet parameters
+        self.sprite_sheet = SpriteSheet(sprite_sheet_img)
+
+        # sprite parameters
+        self.sprite_width = width
+        self.sprite_height = height
+        self.sprite_scale = scale
+
+        # animation parameters
+        self.animation_list = []
+        self.animation_steps = frame_counts  # list of frame counts for every animation in the sprite sheet
+        self.action = 0
+        self.last_update = pygame.time.get_ticks()
+        self.animation_cooldown = frame_time
+        self.frame = 0
+
+        step_counter = 0
+        for animation in self.animation_steps:
+            temp_img_list = []
+            for _ in range(animation):
+                temp_img_list.append(
+                    self.sprite_sheet.get_image(step_counter, self.sprite_width, self.sprite_height,
+                                                self.sprite_scale,
+                                                RGB.KEY))
+                step_counter += 1
+            self.animation_list.append(temp_img_list)
+
+    def update(self):
+        current_time = pygame.time.get_ticks()
+        if current_time - self.last_update >= self.animation_cooldown:
+            self.frame += 1
+            self.last_update = current_time
+            if self.frame >= len(self.animation_list[self.action]):
+                frame = 0
+
+    def draw(self, screen: pygame.Surface, coords: tuple):
+        screen.blit(self.animation_list[self.action][self.frame], coords)
--- src/colors.py
+++ src/colors.py
@@ -1,22 +1,21 @@
+from enum import Enum
+
 Hell = ValueError
 
-class RGB:
-    def __init__(self):
-        orange = self.hex_color_to_tuple("#ffa000")
-        pass    
-    BLACK = (0,0,0)
+
+def hex_color_to_tuple(erm: str):
+    if erm[0] == '#':
+        if len(erm) == 7:
+            return int(erm[1]) * 16 + int(erm[2]), int(erm[3]) * 16 + int(erm[4]), int(erm[5]) * 16 + int(erm[6])
+    else:
+        raise Hell("pass #xxyyzz or #xxyyzzaa(not implemented >_<) here")
+
+
+class RGB(Enum):
+    BLACK = (0, 0, 0)
     BLUE = (0, 0, 255)
     RED = (255, 0, 0)
     GREEN = (0, 255, 0)
     WHITE = (255, 255, 255)
-
-    def hex_color_to_tuple(self,erm : str):
-        if(erm[0]=='#'):
-            if(len(erm) == 7):
-                return (hex(erm[1]+erm[2]), hex(erm[3]+erm[4]), hex(erm[5] + erm[6]))
-        else:
-            raise Hell("pass #xxyyzz or #xxyyzzaa(not implemented >_<) here")
-
-
-
-
+    ORANGE = hex_color_to_tuple("#ffa000")
+    KEY = hex_color_to_tuple("#00b140")
